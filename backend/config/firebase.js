512ccc1123ab6f93878779cda8d04c813c7f25e9
Shravan Jain <shravan.jain1210@gmail.com>
2026-09-18 21:55:42 +0530

    made changes for render 2

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,10 +1,12 @@
 const admin = require("firebase-admin");
+const path = require("path");
+
 const serviceAccount = process.env.RENDER
     ? require("/etc/secrets/firebase-service-account.json")
-    : require("./firebase-service-account.json");
+    : require(path.join(__dirname, "firebase-service-account.json"));
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
