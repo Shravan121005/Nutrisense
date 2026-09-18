71414beb5f37188f7ace745be7def049896928f2
Shravan Jain <shravan.jain1210@gmail.com>
2026-09-18 21:51:55 +0530

    made changes for render

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,5 +1,7 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = process.env.RENDER
+    ? require("/etc/secrets/firebase-service-account.json")
+    : require("./firebase-service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
